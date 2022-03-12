c593d93ddf8886a7f30078b4bca104e2f342a612
sakdt <sakdt5@web.de>
2022-03-12 16:06:12 +0100

    Data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Eshop1';
+  person: string='mirja';
+  age: number=23;
+  address:any={street:'rue du ...', city:'Ariana'};
+  hide:boolean= false;
+  hideInter:boolean= false;
+  activated:boolean=false;
+  getEmail(){
+    return this.person + '@gmail.com';
+  }
+  hideInterpolation(){
+      this.hideInter= !this.hideInter;
+  }
 }
